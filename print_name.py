f187f752d8963b9c423bf332766700d25aab3d38
Danilo Madrigalejos <wenkaru@yaho.com>
2024-03-23 17:05:11 +0800

    Print name in fancy way

--- print_name.py
+++ print_name.py
@@ -1,3 +1,5 @@
+from art import *
+
 # pseudocode
 
 # ask user to input name
@@ -5,5 +7,10 @@ name = input("Please input your name: ")
 
 # ask user to input dream job
 job = input("Please input your dream job: ")
+
 # print name in fancy way
+
+name_art = text2art(name)
+print(name_art)
+
 # print dream jon in fancy way
